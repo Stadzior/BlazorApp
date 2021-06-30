b09559f0dd664ce6e1da6c06db306b9e31f68f36
Kamil Stadryniak <kamil.stadryniak@ttpsc.pl>
2021-06-30 09:38:26 +0200

    Added new cases for UI Testing.

--- BlazorApp.UITests/CounterTests.cs
+++ BlazorApp.UITests/CounterTests.cs
@@ -22,5 +22,49 @@ namespace BlazorApp.UITests
 
             session.Driver.FindElement(By.Id("counter_value"), 10).Text.Should().Be("Current count: 1");
         }
+
+        [Test]
+        public void UserClicksTheDecrementValueButtonTest()
+        {
+            using var session = new WebSession(BrowserType.Chrome);
+
+            session.Driver.Navigate().GoToUrl("http://localhost:5000");
+            session.Driver.FindElement(By.Id("counter_menu_link"), 10).Click();
+            session.Driver.FindElement(By.Id("decrement_count_button"), 10).Click();
+
+            session.Driver.FindElement(By.Id("counter_value"), 10).Text.Should().Be("Current count: 0");
+        }
+
+        [Test]
+        public void UserClicksIncrementValueButtonTwiceAndThenTheDecrementValueButtonTest()
+        {
+            using var session = new WebSession(BrowserType.Chrome);
+
+            session.Driver.Navigate().GoToUrl("http://localhost:5000");
+            session.Driver.FindElement(By.Id("counter_menu_link"), 10).Click();
+
+            var incrementCountButton = session.Driver.FindElement(By.Id("increment_count_button"), 10);
+            incrementCountButton.Click();
+            incrementCountButton.Click();
+            session.Driver.FindElement(By.Id("decrement_count_button"), 10).Click();
+
+            session.Driver.FindElement(By.Id("counter_value"), 10).Text.Should().Be("Current count: 1");
+        }
+
+        [Test]
+        public void UserClicksIncrementValueButtonTwiceAndThenResetButtonTest()
+        {
+            using var session = new WebSession(BrowserType.Chrome);
+
+            session.Driver.Navigate().GoToUrl("http://localhost:5000");
+            session.Driver.FindElement(By.Id("counter_menu_link"), 10).Click();
+
+            var incrementCountButton = session.Driver.FindElement(By.Id("increment_count_button"), 10);
+            incrementCountButton.Click();
+            incrementCountButton.Click();
+            session.Driver.FindElement(By.Id("reset_count_button"), 10).Click();
+
+            session.Driver.FindElement(By.Id("counter_value"), 10).Text.Should().Be("Current count: 0");
+        }
     }
 }
